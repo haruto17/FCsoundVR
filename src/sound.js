392a0891aed4d3ba0586d1148d5dbd3e5fe232c6
haruto17 <s1902272@sendai-nct.jp>
2024-03-19 12:31:28 +0900

    音鳴らす

--- src/sound.js
+++ src/sound.js
@@ -1,5 +1,32 @@
 let osc = [];
 
-function playSound() {}
+init();
+register();
 
-function stopSound() {}
+function init() {
+  for (let i = 0; i < 13; i++) {
+    let f = 261.626 * (2 ** 0.083333) ** i;
+    osc[i] = new Tone.Oscillator(f, "square").toDestination();
+  }
+}
+
+function register() {
+  const ButtonEls = document.querySelectorAll("[button]");
+  for (let i = 0; i < ButtonEls.length; i++) {
+    let el = ButtonEls[i];
+    el.addEventListener("pressedstarted", () => {
+      playSound(i);
+    });
+    el.addEventListener("pressedended", () => {
+      stopSound(i);
+    });
+  }
+}
+
+function playSound(index) {
+  osc[index].start();
+}
+
+function stopSound(index) {
+  osc[index].stop();
+}
